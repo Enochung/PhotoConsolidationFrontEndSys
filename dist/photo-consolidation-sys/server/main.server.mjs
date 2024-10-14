46b8db43b337ac9bf2fab942f6c995ef9825843c
Enochung <hereweare4834@gmail.com>
2024-10-14 16:44:57 +0800

    set -u

---
+++ dist/photo-consolidation-sys/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ODLNMFWF.mjs";import"./chunk-76QUA6YX.mjs";import"./chunk-XOMP56UV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/@Service/HttpService.ts
+++ src/@Service/HttpService.ts
@@ -10,7 +10,8 @@ export class HttpService implements IHttpService {
   constructor(private httpClient: HttpClient) { }
 
   private baseUrl: string = 
-  'http://127.0.0.1:5000/';
+  'https://rep.cgsh.tc.edu.tw/flaskapi/api/';
+  //'http://127.0.0.1:5000/';
 
   async getRequest<T>(urlstr: string): Promise<T> {
     const observable = this.httpClient.get<T>(this.baseUrl + urlstr);
